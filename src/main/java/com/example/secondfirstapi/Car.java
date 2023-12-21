153e15e9fc8f0111eacfefa5938f8d110766862f
Alban Gashi <albangashi@Albans-MacBook-Pro.local>
2023-12-21 20:57:55 +0100

    Remove redundant imports

--- src/main/java/com/example/secondfirstapi/Car.java
+++ src/main/java/com/example/secondfirstapi/Car.java
@@ -1,6 +1,5 @@
 package com.example.secondfirstapi;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
